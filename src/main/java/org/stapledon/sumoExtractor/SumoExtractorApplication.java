3507dae05c398ffee0ca262cc8bb92a9377b9328
Adrian Gilbert <adrian@gilbert.ca>
2022-12-22 09:15:08 -0500

    Spring Initializr

---
+++ src/main/java/org/stapledon/sumoExtractor/SumoExtractorApplication.java
@@ -0,0 +1,13 @@
+package org.stapledon.sumoExtractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumoExtractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumoExtractorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/stapledon/sumoExtractor/SumoExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.stapledon.sumoExtractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumoExtractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
